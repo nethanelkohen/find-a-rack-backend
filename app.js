21740c008e48ea9ccd82664dac3b5f38d10f324e
Nethanel Kohen <nethanel.kohen@gmail.com>
2018-11-11 21:25:06 -0500

    conditionals for data

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(function(req, res, next) {
     global.connection = mysql.createConnection({
       host: "localhost",
       user: "root",
-      password: "",
+      password: "password",
       database: "racks"
     });
   } else {
--- routes/update.js
+++ routes/update.js
@@ -1,14 +1,26 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-
 router.post("/", function(req, res, next) {
+  if (
+    !req.body.name ||
+    !req.body.address ||
+    !req.body.value ||
+    !req.body.locationLatitude ||
+    !req.body.locationLongitude
+  ) {
+    return res.status(500).send("Make sure you've added every value");
+  }
+
+  if (req.body.name.length < 3) {
+    return res.status(500).send("Name can't be less than 3 characaters");
+  }
+
   let query =
     "INSERT INTO `racks` (name, address, value, location_latitude, location_longitude) VALUES ('" +
-    req.body.name +
+    req.body.name.toUpperCase() +
     "', '" +
-    req.body.address +
+    req.body.address.toUpperCase() +
     "', '" +
     req.body.value +
     "', '" +
@@ -21,7 +33,7 @@ router.post("/", function(req, res, next) {
     if (err) {
       return res.status(500).send(err);
     }
-    res.send("Hello");
+    res.send("Row added");
     connection.end();
   });
 });
